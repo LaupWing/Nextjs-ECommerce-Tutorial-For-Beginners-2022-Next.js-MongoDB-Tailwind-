dd974d1a684565e5b95e18ea3d1ff4038c570447
LaupWing <loc_strict@hotmail.com>
2022-10-27 09:44:06 +0200

    description

--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -30,7 +30,13 @@ const ProductPage = () => {
             </div>
             <div>
                <ul>
-                  <li></li>
+                  <li>
+                     <h1 className="text-lg">{product.name}</h1>
+                  </li>
+                  <li>Category: {product.category}</li>
+                  <li>Brand: {product.brand}</li>
+                  <li>{product.rating} of {product.numReviews} reviews</li>
+                  <li>Description: {product.description}</li>
                </ul>
             </div>
          </div>
