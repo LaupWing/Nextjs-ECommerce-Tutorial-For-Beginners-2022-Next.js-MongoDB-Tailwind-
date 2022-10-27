0ccea790a8618b77aa9fc1b77de792309bd3ffb3
LaupWing <loc_strict@hotmail.com>
2022-10-27 18:52:13 +0200

    t head

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -18,7 +18,18 @@ const CartPage = () => {
                <div>Cart is empty. <Link href={"/"}>Go Shopping</Link></div>
             ) : (
                <div className="grid md:grid-cols-4 md:gap-5">
-                  <div className="overflow-x-auto md:col-span-3"></div>
+                  <div className="overflow-x-auto md:col-span-3">
+                     <table className="min-w-full">
+                        <thead className="border-b">
+                           <tr>
+                              <th className="px-5 text-left">Item</th>
+                              <th className="p-5 text-right">Quantity</th>
+                              <th className="p-5 text-right">Price</th>
+                              <th className="p-5">Action</th>
+                           </tr>
+                        </thead>
+                     </table>
+                  </div>
                </div>
             ) 
          }
