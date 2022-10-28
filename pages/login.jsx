3cc9838db071215866f199b23c64578d4fc39f77
LaupWing <loc_strict@hotmail.com>
2022-10-28 09:41:23 +0200

    forma handler

--- pages/login.jsx
+++ pages/login.jsx
@@ -10,15 +10,15 @@ const LoginPage = () => {
       formState: { errors }
    } = useForm()
 
-   const submitHandler = ({ }) => {
-
+   const submitHandler = ({ email, password }) => {
+      console.log(email, password)
    }
 
    return (
       <Layout title={"Login"}>
          <form
             className="mx-auto max-w-screen-md"
-            onSubmit={handleSubmit()}
+            onSubmit={handleSubmit(submitHandler)}
          >
             <h1 className="mb-4 text-xl">Login</h1>
             <div className="mb-4">
@@ -40,7 +40,20 @@ const LoginPage = () => {
             </div>
             <div className="mb-4">
                <label htmlFor="password">Password</label>
-               <input type="password" className="w-full" id="password" autoFocus />
+               <input 
+                  type="password" 
+                  className="w-full" 
+                  id="password" 
+                  autoFocus 
+                  {...register("password", {
+                     required: "Please enter password",
+                     minLength: { 
+                        value: 6, 
+                        message: "Password has to be more than 3 characters"
+                  }
+                  })}
+               />
+               {errors.password && (<div className="text-red-500">{errors.password.message}</div>)}
             </div>
             <div className="mb-4">
                <button className="primary-button">Login</button>
