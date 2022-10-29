6ad33708402bc4c31f85d1972dbe0fdfccead25d
LaupWing <loc_strict@hotmail.com>
2022-10-29 09:34:10 +0200

    update

---
+++ components/CheckoutWizard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const CheckoutWizard = ({activeStep = 0}) => {
+   return (
+      <div className="mb-4 flex flex-wrap">
+         {
+            ["User Login", "Shipping Address", "Payment Method", "Place Order"].map((step, index)=>(
+               <div
+                  key={index}
+                  className={`flex-1 border-2 text-center ${index <= activeStep ? "border-indigo-500 text-indigo-500" : "border-gray-400 text-gray-400"}`}
+               >
+                  {step}
+               </div>
+            ))
+         }
+      </div>
+   )
+}
+
+export default CheckoutWizard
