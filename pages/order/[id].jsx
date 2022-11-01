3bd746c029fc3303de69176078b5a39f2b066fff
LaupWing <loc_strict@hotmail.com>
2022-11-01 17:20:23 +0100

    T.T so boring

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -61,6 +61,20 @@ const OrderPage = () => {
       error: '',
    })
 
+   const {
+      shippingAddress,
+      paymentMethod,
+      orderItems,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+      isPaid,
+      paidAt,
+      isDelivered,
+      deliveredAt,
+   } = order
+
    useEffect(() => {
       const fetchOrder = async () => {
          try {
@@ -78,6 +92,55 @@ const OrderPage = () => {
          fetchOrder()
       }
    }, [orderId, order])
+
+   function createOrder(data, actions) {
+      return actions.order
+         .create({
+            purchase_units: [
+               {
+                  amount: { value: totalPrice },
+               },
+            ],
+         })
+         .then((orderID) => {
+            return orderID;
+         });
+   }
+
+   function onApprove(data, actions) {
+      return actions.order.capture().then(async function (details) {
+         try {
+            dispatch({ type: 'PAY_REQUEST' });
+            const { data } = await axios.put(
+               `/api/orders/${order._id}/pay`,
+               details
+            );
+            dispatch({ type: 'PAY_SUCCESS', payload: data });
+            toast.success('Order is paid successgully');
+         } catch (err) {
+            dispatch({ type: 'PAY_FAIL', payload: getError(err) });
+            toast.error(getError(err));
+         }
+      });
+   }
+   function onError(err) {
+      toast.error(getError(err));
+   }
+
+   async function deliverOrderHandler() {
+      try {
+         dispatch({ type: 'DELIVER_REQUEST' });
+         const { data } = await axios.put(
+            `/api/admin/orders/${order._id}/deliver`,
+            {}
+         );
+         dispatch({ type: 'DELIVER_SUCCESS', payload: data });
+         toast.success('Order is delivered');
+      } catch (err) {
+         dispatch({ type: 'DELIVER_FAIL', payload: getError(err) });
+         toast.error(getError(err));
+      }
+   }
 
    return (
       <Layout title={`Order ${orderId}`}>
